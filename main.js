ebc730c8da1c697cd9f5423ed3b9b1ec377d17ef
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2017-12-06 18:39:55 -0700

    adding code

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ var x= "x";
 var o= "o";
 var counter = 0
 
-$( ".celldata" ).click(function(){
 
-	if (counter % 2 == 0) { 
+$( ".celldata" ).click(function(){ //this line says that whenever a click is performed in class of celldata to run a function
+
+	if (counter % 2 == 0) { //this ch
 		$(this).html(x);
 		counter++;
 	}
@@ -16,6 +17,9 @@ $( ".celldata" ).click(function(){
     }
 });
 
+$('button').click(function() {
+    location.reload();
+});
 
 
 
