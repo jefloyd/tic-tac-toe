791aa99d65a1a1c02be135f48b7d819c8f796c39
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2017-12-06 19:54:18 -0700

    tic tac hoe amiright

--- main.js
+++ main.js
@@ -15,10 +15,43 @@ $( ".celldata" ).click(function(){ //this line says that whenever a click is per
    		$(this).html(o);
    		counter++;
     }
+
+    
+    if($('#1').html() == x && $('#2').html() == x && $('#3').html() == x){
+    	alert("you win");
+    }
+     if($('#4').html() == x && $('#5').html() == x && $('#6').html() == x){
+    	alert("you win");
+    }
+    if($('#7').html() == x && $('#8').html() == x && $('#9').html() == x){
+    	alert("you win");
+    }
+
+  
+
+
+
+
+
+
+  
 });
+
+
+  //need to make if/then statement for all rows/columns to check if 123=x alert WIN etc...
+    /* if("#1").innerHTML == x && ("#2").innerHTML == x && ("3").innerHTML == x;
+    if("#4" == x && "#5" == x && "6" == x);
+    if("#7" == x && "#8" == x && "9" == x);
+    	alert("You Win!")  */
+
+	//need to make if/then statement for all rows/columns to check if 123=x alert WIN etc...
+
+
 
 $('button').click(function() {
     location.reload();
+
+
 });
 
 
